6907c3b3ff0699dd060233cf8d212e21a8961879
Sevada <sevada@lsones.eu>
2019-04-28 18:41:24 +0400

    Added include to lowercase checkup

--- controllers/events/get-with-filter.js
+++ controllers/events/get-with-filter.js
@@ -1,6 +1,7 @@
 import Event from '../../models/event'
 import Group from '../../models/group'
 import log from '../../utils/log'
+import db from '../../components/db-config/index'
 
 export const handler = (req, res) => new Promise(async() => {
   try {
@@ -8,16 +9,15 @@ export const handler = (req, res) => new Promise(async() => {
     if (!offset || !limit || (!city && !country)) throw new Error('Missing query params')
 
     let queryObject = {}
-    if (country) queryObject.country = { $like: `%${country}%` }
-    if (city) queryObject.city = { $like: `%${city}%` }
-
+    if (country) queryObject.country = country;
+    if (city) queryObject.city = db.Sequelize.where(db.Sequelize.fn('LOWER', db.Sequelize.col('city')), 'LIKE', '%' + city + '%');
     const events = await Event.findAll({
       where: queryObject,
       offset: Number(offset),
       limit: Number(limit),
       order: [[ 'created','ASC']],
       include: 
-      [{ model: Group }]
+      [{model: Group}]
     })
 
     res.json({
